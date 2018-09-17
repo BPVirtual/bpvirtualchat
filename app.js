c80fc4252ec1da679a7043962817137b75ce5f42
Renan Tavares <renan_tsm@hotmail.com>
2018-09-17 18:00:33 +0000

    Alteração para teste POST de feedbacks

--- app.js
+++ app.js
@@ -73,7 +73,8 @@ app.get('/feedback/:cpf', (req, res) => {
 });
 
 app.post('/feedback', (req, res) => {
-  var myobj = [{ cpf: req.body.cpf, nota: req.body.nota }];
+  var myobj = [{ _id: new mongoose.Types.ObjectId(), cpf: req.body.cpf, nota: req.body.nota }];
+  return myobj;
   dbo.collection("feedbacks").insertMany(myobj, function(err, res) {
     if (err) throw err;
     console.log(res.insertedCount + " documents inserted");
