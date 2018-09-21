7bcd2366c9db83e30cb6bb15afc368b121429ceb
Renan Tavares <renan_tsm@hotmail.com>
2018-09-21 13:32:12 -0300

    Ajuste no template para header personalizado

--- script/template.js
+++ script/template.js
@@ -1,31 +1,70 @@
-var json2csv = require('json2csv').parse;
  
 exports.get = function(req, res) {
  
-    var fields = [
-        'cpf',
-        'nome',
-        'sobrenome',
-        'nacionalidade',
-        'dataNascimento',
-        'sexo',
-        'estadoCivil',
-        'profissao',
-        'cargo',
-        'dataAdmissao',
-        'salario',
-        'endereco.logradouro',
-        'endereco.bairro',
-        'endereco.cep',
-        'endereco.cidade',
-        'endereco.uf',
-        'empresa.razaoSocial',
-        'empresa.cnpj',
-        'consultor.nome',
-        'consultor.email'
-    ];
-
-    var json = {
+	const Json2csvParser = require('json2csv').Parser;
+	const fields = [{
+	  label: 'CPF',
+	  value: 'cpf'
+	},{
+	  label: 'Nome',
+	  value: 'nome'
+	},{
+	  label: 'Sobrenome',
+	  value: 'sobrenome'
+	},{
+	  label: 'Nacionalidade',
+	  value: 'nacionalidade'
+	},{
+	  label: 'Data de Nascimento',
+	  value: 'dataNascimento'
+	},{
+	  label: 'Sexo',
+	  value: 'sexo'
+	},{
+	  label: 'Estado Civil',
+	  value: 'estadoCivil'
+	},{
+	  label: 'Profissão',
+	  value: 'profissao'
+	},{
+	  label: 'Cargo',
+	  value: 'cargo'
+	},{
+	  label: 'Data de Admissão',
+	  value: 'dataAdmissao'
+	},{
+	  label: 'Salário',
+	  value: 'salario'
+	},{
+	  label: 'Endereço - Logradouro',
+	  value: 'endereco.logradouro'
+	},{
+	  label: 'Endereço - Bairro',
+	  value: 'endereco.bairro'
+	},{
+	  label: 'Endereço - CEP',
+	  value: 'endereco.cep'
+	},{
+	  label: 'Endereço - Cidade',
+	  value: 'endereco.cidade'
+	},{
+	  label: 'Endereço - UF',
+	  value: 'endereco.uf'
+	},{
+	  label: 'Empresa - Razão Social',
+	  value: 'empresa.razaoSocial'
+	},{
+	  label: 'Empresa - CNPJ',
+	  value: 'empresa.cnpj'
+	},{
+	  label: 'Consultor - Nome',
+	  value: 'consultor.nome'
+	},{
+	  label: 'Consultor - Email',
+	  value: 'consultor.email'
+	}];
+	
+	var json = [{
         'cpf': '12345678900',
         'nome': 'nomeProponente',
         'sobrenome': 'sobrenomeProponente',
@@ -46,13 +85,14 @@ exports.get = function(req, res) {
         'empresa.cnpj': '12345678901234',
         'consultor.nome': 'seuNome',
         'consultor.email': 'seuEmail'
-    }
- 
-    var csv = json2csv(json, fields);
- 
-    res.set("Content-Disposition", "attachment;filename=proponentes.csv");
-    res.set("Content-Type", "application/octet-stream");
- 
-    res.send(csv);
- 
+    }];
+	 
+	const json2csvParser = new Json2csvParser({ fields, quote: '', delimiter: ';' });
+	const csv = json2csvParser.parse(json);
+
+	res.set('Content-Disposition', 'attachment;filename=proponentes.csv');
+	res.set('Content-Type', 'application/octet-stream');
+
+	res.send(csv);
+
 };
