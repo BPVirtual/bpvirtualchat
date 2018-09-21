78c273644ef978f8f450c0b2e69262c25fd02592
Renan Tavares <renan_tsm@hotmail.com>
2018-09-21 13:57:45 -0300

    Voltando ao original

--- script/template.js
+++ script/template.js
@@ -1,31 +1,31 @@
+var json2csv = require('json2csv').parse;
  
 exports.get = function(req, res) {
  
-	const Json2csvParser = require('json2csv').Parser;
-	var fields = [
-		'cpf',
-		'nome',
-		'sobrenome',
-		'nacionalidade',
-		'dataNascimento',
-		'sexo',
-		'estadoCivil',
-		'profissao',
-		'cargo',
-		'dataAdmissao',
-		'salario',
-		'endereco.logradouro',
-		'endereco.bairro',
-		'endereco.cep',
-		'endereco.cidade',
-		'endereco.uf',
-		'empresa.razaoSocial',
-		'empresa.cnpj',
-		'consultor.nome',
-		'consultor.email'
-    	];
-	
-	var json = [{
+    var fields = [
+        'cpf',
+        'nome',
+        'sobrenome',
+        'nacionalidade',
+        'dataNascimento',
+        'sexo',
+        'estadoCivil',
+        'profissao',
+        'cargo',
+        'dataAdmissao',
+        'salario',
+        'endereco.logradouro',
+        'endereco.bairro',
+        'endereco.cep',
+        'endereco.cidade',
+        'endereco.uf',
+        'empresa.razaoSocial',
+        'empresa.cnpj',
+        'consultor.nome',
+        'consultor.email'
+    ];
+
+    var json = {
         'cpf': '12345678900',
         'nome': 'nomeProponente',
         'sobrenome': 'sobrenomeProponente',
@@ -46,14 +46,13 @@ exports.get = function(req, res) {
         'empresa.cnpj': '12345678901234',
         'consultor.nome': 'seuNome',
         'consultor.email': 'seuEmail'
-    }];
-	 
-	const json2csvParser = new Json2csvParser({ fields, quote: '', delimiter: ';' });
-	const csv = json2csvParser.parse(json);
-
-	res.set('Content-Disposition', 'attachment;filename=proponentes.csv');
-	res.set('Content-Type', 'application/octet-stream');
-
-	res.send(csv);
-
+    }
+ 
+    var csv = json2csv(json, fields);
+ 
+    res.set("Content-Disposition", "attachment;filename=proponentes.csv");
+    res.set("Content-Type", "application/octet-stream");
+ 
+    res.send(csv);
+ 
 };
